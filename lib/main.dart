de27ea33a531a6204947e48063c0008e902cce59
SatnaSetiyana <setianasatna@gmail.com@gmail.com>
2024-05-20 19:07:12 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hallo Dunia',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MahasiswaList(),
+    );
+  }
+}
+
+class MahasiswaList extends StatefulWidget {
+  @override
+  _MahasiswaListState createState() => _MahasiswaListState();
+}
+
+class _MahasiswaListState extends State<MahasiswaList> {
+  List<Map<String, dynamic>> _mahasiswa = [
+    {'nama': 'SATNA SETIYANA', 'warna': Color.fromARGB(255, 51, 7, 122)},
+    {'nama': 'ADI LUKMAN NURHAKIM', 'warna': Colors.orange},
+    {'nama': 'WILSYA NURALISA', 'warna': Colors.red},
+    {'nama': 'ALIEF WAHYULIANTO', 'warna': Colors.green},
+    {'nama': 'AURELIA DHEA SYAFENIA', 'warna': Colors.pink},
+  ];
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Apa Kabar'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RichText(
+              text: TextSpan(
+                text: _mahasiswa[_currentIndex]['nama'],
+                style: TextStyle(
+                    fontSize: 24,
+                    color: _mahasiswa[_currentIndex]['warna'].withOpacity(0.8)),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _currentIndex = (_currentIndex + 1) % _mahasiswa.length;
+                });
+              },
+              child: Text('lanjut'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
